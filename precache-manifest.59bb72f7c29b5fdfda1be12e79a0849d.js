0dfbf3f1b43f1e655434c49d9bb8bdd6240fc22e
Roy Flores <roy10290@gmail.com>
2020-04-04 15:15:55 -0500

    Updates

---
+++ precache-manifest.59bb72f7c29b5fdfda1be12e79a0849d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d21f58f13843b43ff2c9e8fc337e60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7374962d4c565291614f",
+    "url": "/static/css/main.7058c6b5.chunk.css"
+  },
+  {
+    "revision": "4f23b1610b001386f196",
+    "url": "/static/js/2.05604367.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05604367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7374962d4c565291614f",
+    "url": "/static/js/main.b4826a6f.chunk.js"
+  },
+  {
+    "revision": "9f1375a7a439b42c406f",
+    "url": "/static/js/runtime-main.78c6ea5b.js"
+  }
+]);
